12b1181f55c36d28f60a0ce9d5d3c7c02c5d8ba9
Hackerboy321 <deepanshukhanktriyal4@gmail.com>
2026-01-06 22:55:01 +0530

    day 2

---
+++ Trees/1_Binary_Tree.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <queue> 
+using namespace std ;
+
+struct node 
+{
+    int data ;
+    node* left ;
+    node* right ;
+
+    node(int value )
+    {
+        data = value ;
+        left = right = NULL ;
+    }
+};
+
+void inorder(node* rootNode)
+{
+    if(rootNode == NULL) return ; 
+
+    inorder(rootNode->left) ; 
+    cout<<rootNode->data ;
+    inorder(rootNode->right) ; 
+}
+
+int main()
+{
+    int x ; 
+    cout<<"Enter the root element : " ; 
+    cin>>x ; 
+    int left_child , right_child ;
+
+    queue<node*> q ;
+    node* root = new node(x) ;
+    q.push(root) ;  
+
+    while(!q.empty())
+    {
+        node* temp = q.front() ; 
+        q.pop() ; 
+
+        
+        // left child 
+        cout<<"Enter value of"<<temp->data<<"Left_child : " ; 
+        cin>>left_child ; 
+
+        if(left_child != -1)
+        {
+            temp->left = new node(left_child) ; 
+            q.push(temp->left) ; 
+        }
+
+        
+        // Right child 
+        cout<<"Enter value of"<<temp->data<<" Right_child : " ; 
+        cin>>right_child; 
+
+        if(right_child != -1) 
+        {
+            temp->right = new node(right_child) ; 
+            q.push(temp->right) ; 
+        }
+
+    }
+}
--- rough.cpp
+++ rough.cpp
@@ -1,26 +1,66 @@
 #include <iostream>
-using namespace std ;
+#include <queue>
+using namespace std ; 
 
+struct node
+{
+    int data ; 
+    node* left ; 
+    node* right ; 
+
+    node( int value )
+    {
+        data = value ; 
+        left = right = NULL ; 
+    }
+}; 
+
+void inorder( node* root)
+{
+    if(root == NULL ) return ; 
+
+    inorder(root->left) ; 
+    cout<<root->data; 
+    inorder(root->right) ; 
+}
 
 int main()
 {
-    int arr[] = {4,2,1,3,5,4};
-    int n = sizeof(arr)/sizeof(int) ; 
+    int n, right_node , left_node ; 
+    cout<<"Enter the root node value : " ;
+    cin>>n; 
+ 
+    queue<node*> q ; 
+    node* root = new node(n) ; 
+    q.push(root) ; 
 
-   for(int i = 0 ;  i < n -1; i++)
-   {
-        int minIdx = i ; 
+    while(!q.empty())
+    {
+        node* temp = q.front() ; 
+        q.pop() ; 
 
-        for(int j =  i+1  ; j < n ; j++ )
+        // for left child 
+        cout<<"Enter the value of left child node of "<<temp->data<<" : ";
+        cin>>left_node ;
+
+        if(left_node != -1 )
         {
-            if(arr[minIdx] > arr[j]) ; minIdx = j ; 
+            temp->left = new node(left_node) ; 
+            q.push(temp->left) ; 
         }
 
-        swap(arr[minIdx] , arr[i]) ; 
-   }
 
-    for(int i = 0 ; i < n ; i++)
-    {
-        cout<<arr[i] ;
+        //for right child 
+        cout<<"Enter the value of right childe node of "<<temp->data<<" : ";  
+        cin>>right_node ; 
+
+        if(right_node != -1 )
+        {
+            temp->right = new node(right_node) ; 
+            q.push(temp->right) ; 
+        }
     }
+
+    inorder(root) ;
+
 }
